bae9169e3ba819667ab6224f7f4a6584d4356662
Mark Jerde <mark.a.jerde@gmail.com>
2017-10-19 14:41:24 -0500

    Change saveForgottenClippings from acting only when a clipping is added, and sometimes saving when a duplicate is ignored and thus nothing forgotten, to saving when informed by the store that something is being forgotten.  Includes safeguards to not save a forgoten clipping if the user initiated the forgetting via delete or save-and-delete, or when a clipping is moved to the favorites store.

--- FlycutEngine/FlycutStore.m
+++ FlycutEngine/FlycutStore.m
@@ -115,6 +115,7 @@
 
 	// Delete clippings older than jcRememberNum
 	while ( [jcList count] > jcRememberNum ) {
+		[self delegateWillDeleteClippingAtIndex:jcRememberNum];
 		[jcList removeObjectAtIndex:jcRememberNum];
 		[self delegateDeleteClippingAtIndex:(jcRememberNum-1)]; // -1 for before-add indexing
 	}
@@ -132,7 +133,10 @@
     [self delegateBeginUpdates];
 
     for ( int i = (int)[jcList count] ; i > 0 ; i-- )
+	{
+		[self delegateWillDeleteClippingAtIndex:(i-1)];
         [self delegateDeleteClippingAtIndex:(i-1)];
+	}
 
     NSMutableArray *emptyJCList;
     emptyJCList = [[NSMutableArray alloc] init];
@@ -151,6 +155,7 @@
 {
     [self delegateBeginUpdates];
 
+	[self delegateWillDeleteClippingAtIndex:index];
     [jcList removeObjectAtIndex:index];
     [self delegateDeleteClippingAtIndex:index];
 
@@ -184,6 +189,7 @@
 			[self delegateBeginUpdates];
 
 			while ( [jcList count] > jcRememberNum ) {
+				[self delegateWillDeleteClippingAtIndex:jcRememberNum];
 				[jcList removeObjectAtIndex:jcRememberNum];
 				[self delegateDeleteClippingAtIndex:jcRememberNum];
 			}
@@ -378,6 +384,12 @@
 	modifiedSinceLastSaveStore = YES;
 	if ( self.delegate && [self.delegate respondsToSelector:@selector(insertClippingAtIndex:)] )
 		[self.delegate insertClippingAtIndex:index];
+}
+
+-(void) delegateWillDeleteClippingAtIndex:(int)index
+{
+	if ( self.deleteDelegate && [self.deleteDelegate respondsToSelector:@selector(willDeleteClippingFromStore:AtIndex:)] )
+		[self.deleteDelegate willDeleteClippingFromStore:self AtIndex:index];
 }
 
 -(void) delegateDeleteClippingAtIndex:(int)index
